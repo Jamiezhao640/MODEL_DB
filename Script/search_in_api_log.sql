6d70ea39675c6baf589cd9966006a74211a722e8
jamie <jamie.zhao@navidata.ca>
2023-06-11 00:04:34 -0700

    main change

--- Script/search_in_api_log.sql
+++ Script/search_in_api_log.sql
@@ -3,3 +3,5 @@ select * from tbl_api_log
 where ISJSON(api_json) =1 and exists (select * from openjson (api_json) where [key] = 'resource_name' and [value] = 'sp_new_po')
 
 
+this is in mmain branch
+
